4c1f5547adca519bece90510b7d3a54cbeff2669
Anis Tariq <anistariq@hotmail.com>
2021-11-09 14:11:31 -0500

    Updated search button

--- ourJS/search.js
+++ ourJS/search.js
@@ -1,15 +1,30 @@
+const searchList = document.getElementById('searchList');
+
+const loadSearch = async () => {
+    try{
+        const res = await fetch('');
+        let hpSearch = await res.json();
+        displaySearch(hpSearch);
+        console.log(hpSearch);
+    } catch (err){
+        console.error(err);
+    }
+};
+
+
+
 
 const successCallback = (position) => {
 
     console.log(position);
 
-}
+};
 
 const errorCallback = (error) => {
 
     console.error(error);
 
-}
+};
 
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
     enableHighAccuracy: true,
