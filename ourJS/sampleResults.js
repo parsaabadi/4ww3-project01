1e89c8a7ef7f56d65205450ebc1a5009bf9379ed
parsa4ww3 <91708962+parsa4ww3@users.noreply.github.com>
2021-11-12 22:18:38 -0500

    Update sampleResults.js

--- ourJS/sampleResults.js
+++ ourJS/sampleResults.js
@@ -1,15 +1,66 @@
 function initMap() {
-    const pinksLoco = { lat: 43.25769402308975, lng: -79.91881720809519 };
-    const subwayLoco = { lat: 43.25756904970684, lng: -79.91881251180244 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
-      center: pinksLoco,
-      center: subwayLoco,
-    });
+   var options = {
+      zoom:15,
+      center: { lat:43.26103522111267, lng:-79.91918247917609} //Coordinates of Mac
+   }
+var map = new google.maps.Map(document.getElementById('map01'), options);
+var map02 = new google.maps.Map(document.getElementById('map02'), options);
+
+function addMarker(prop) {
+   var marker = new google.maps.Marker({
+      position: prop.coordinates, // Passing the coordinates
+      map:map, //Map that we need to add
+   });
+   if(prop.iconImage) { marker.setIcon(prop.iconImage); }
+   if(prop.content) { 
+      var information = new google.maps.InfoWindow({
+      content: prop.content
+      });
+      
+      marker.addListener('click', function() {
+      information.open(map, marker);
+      });
+   }
    
-    new google.maps.Marker({
-      position: pinksLoco,
-      position: subwayLoco,
-      map,
-    });
+}
+
+function addMarker02(prop) {
+   var marker = new google.maps.Marker({
+      position: prop.coordinates, // Passing the coordinates
+      map:map02, //Map that we need to add
+   });
+   if(prop.iconImage) { marker.setIcon(prop.iconImage); }
+   if(prop.content) { 
+      var information = new google.maps.InfoWindow({
+      content: prop.content
+      });
+      
+      marker.addListener('click', function() {
+      information.open(map02, marker);
+      });
+   }
+}
+
+addMarker({
+   coordinates:{lat: 43.25772918691246,lng: -79.91895668252349},
+   content:'<h4>Pinks,1335 Main St W, Hamilton, ON L8S 1C6</h4>'
+});
+
+addMarker({
+   coordinates:{lat: 43.25816, lng: -79.9414},
+  
+   content:'<h4>Subway,119 Osler Dr Unit #11, Dundas, ON L9H6X4</h4>'
+});
+
+addMarker02({
+   coordinates:{lat: 43.25772918691246,lng: -79.91895668252349},
+   content:'<h4>Pinks,1335 Main St W, Hamilton, ON L8S 1C6</h4>'
+});
+
+addMarker02({
+   coordinates:{lat: 43.25816, lng: -79.9414},
+  
+   content:'<h4>Subway,119 Osler Dr Unit #11, Dundas, ON L9H6X4</h4>'
+});
+
 }
