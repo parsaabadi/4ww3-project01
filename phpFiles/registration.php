e80e0fed86ae68c545c36a444e1d4171bf2c0fb4
Anis Tariq <anistariq@hotmail.com>
2021-12-07 13:02:28 -0500

    Updated Registration

--- phpFiles/registration.php
+++ phpFiles/registration.php
@@ -5,7 +5,7 @@
 	$password2 = $_POST['password2'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','APDatabase');
+	$conn = new mysqli('localhost','parsa','parsa','shop');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
