75e77dcd975e09ecc5dea30af16bc23b90f0c2d4
parsa4ww3 <91708962+parsa4ww3@users.noreply.github.com>
2021-12-08 19:00:41 -0500

    Update start.php
    UPDATE COMMENTs

--- phpFiles/start.php
+++ phpFiles/start.php
@@ -1,4 +1,9 @@
 <?php
+#Here we are using config.php to acquire configuration,
+# Then we proceed to use all required dependencies for the AWS bucket,
+# Finally we create an instance for the bucket and set it specification as mentioned before
+
+
 use Aws\S3\S3Client;
 use Aws\Credentials\Credentials;
 require 'vendor/autoload.php';
@@ -8,9 +13,9 @@ $config = require('config.php');
 //s3 instance 
 $s3 = s3Client::factory([
 
-    'version'     => 'latest',
-                'region'      => 'us-east-2',
-                'credentials' => [
+    'version' => 'latest',
+    'region'      => 'us-east-2',
+    'credentials' => [
 
     'key' => $config['s3']['key'],
     'secret' => $config['s3']['secret'],
